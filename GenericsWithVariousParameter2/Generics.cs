b8af2e7726b844752db684bf4f8c7d42e99ac151
adurga-ariqt <adurga@ariqt.com>
2022-11-16 14:16:49 +0530

    Add project files.

---
+++ GenericsWithVariousParameter2/Generics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsWithVariousParameter2
+{
+    public  class Generics
+    {
+        public void Show<T1,T2>(T1 first ,T2 second)
+        {
+            Console.WriteLine("First variable value :{0}\nsecong variable value: {1}\n", first, second);
+        }
+    }
+}
---
+++ GenericsWithVariousParameter2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsWithVariousParameter2;
+
+Console.WriteLine("Hello, World!");
+var exo = new Generics();
+exo.Show(246, "durga");
+exo.Show("bhanu", 34.56);
+exo.Show('d', 256);
